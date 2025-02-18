4e1b26830f5313f95411aa9b8179ab0d003f0e5b
ShadowTheGamer <26454468-ShadowTheGamer@users.noreply.replit.com>
2025-02-18 16:44:19 +0000

    Agent query: I've updated the color picker to change colors in real-time with faster animations. Can you check if the color updates immediately when you move the color picker and if the animation feels smoother?
    Improve color picker responsiveness and animation speed
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/7a39f3f9-3b27-4db6-8c68-459e94d81ef9/9529fba6-a94b-4dc2-adf8-d67d37bd98a9.jpg

--- client/src/components/ui/color-picker.tsx
+++ client/src/components/ui/color-picker.tsx
@@ -12,19 +12,24 @@ const ColorPicker = React.forwardRef<HTMLInputElement, ColorPickerProps>(
   ({ label, className, ...props }, ref) => {
     const [color, setColor] = React.useState(props.defaultValue as string)
 
+    React.useEffect(() => {
+      // Update theme immediately when color changes
+      props.onChange?.({ target: { value: color } } as React.ChangeEvent<HTMLInputElement>)
+    }, [color, props])
+
     return (
       <div className="flex flex-col gap-2">
         {label && <Label>{label}</Label>}
         <div className="flex gap-2 items-center">
           <div
-            className="w-10 h-10 rounded-md border"
+            className="w-10 h-10 rounded-md border transition-colors duration-100"
             style={{ backgroundColor: color }}
           />
           <Input
             ref={ref}
             type="color"
             className={cn(
-              "h-10 p-1 cursor-pointer [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-none",
+              "h-10 p-1 cursor-pointer [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-none transition-all duration-100",
               className
             )}
             value={color}
